a7447bab4ef1e134bb0ac15aaa1675161ecb9097
Brandon Detty <113217431+brandon-detty@users.noreply.github.com>
2023-02-21 13:53:13 -0800

    add Dictionary type [guard]

--- shared/typescript/index.ts
+++ shared/typescript/index.ts
@@ -4,3 +4,5 @@ export * from "./src/Definition.js";
 export { default as Definition } from "./src/Definition.js";
 export * from "./src/DefinitionList.js";
 export { default as DefinitionList } from "./src/DefinitionList.js";
+export * from "./src/Dictionary.js";
+export { default as Dictionary } from "./src/Dictionary.js";
---
+++ shared/typescript/src/Dictionary.ts
@@ -0,0 +1,21 @@
+import Word, { isWord } from "./Word.js";
+import DefinitionList, { isDefinitionList } from "./DefinitionList.js";
+
+export type Dictionary = Record<Word, DefinitionList>;
+export type DictionaryMap = Map<Word, DefinitionList>;
+
+export const isDictionaryRecord = (d: unknown): d is Dictionary => {
+  if (
+    typeof d !== "object" ||
+    d === null ||
+    Array.isArray(d) ||
+    Object.getOwnPropertySymbols(d).length > 0
+  ) {
+    return false;
+  }
+  return Object.getOwnPropertyNames(d).every(
+    (p) => isWord(p) && isDefinitionList(d[p as keyof typeof d])
+  );
+};
+
+export default Dictionary;
