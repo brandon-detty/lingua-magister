9ec34cfb6c50f1fad8034614e382e050a201b6f0
Brandon Detty <113217431+brandon-detty@users.noreply.github.com>
2023-03-04 09:14:05 -0800

    replace manual styles in menu with tailwind classes

--- react/src/RootTemplate.tsx
+++ react/src/RootTemplate.tsx
@@ -7,7 +7,7 @@ const RootTemplate = () => {
   return (
     <>
       <div className="container mx-auto max-w-screen-lg bg-white h-screen">
-        <nav style={{ display: "flex", gap: "1rem", padding: "1rem" }}>
+        <nav className={`flex gap-4 ${t.padding.p}`}>
           <Link to="/">Home</Link>
           <Link to="/library">Library</Link>
         </nav>
